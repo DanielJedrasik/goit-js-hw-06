2af48395a2cebbd7022e9b9821b4ebdeada4a598
DanielJedrasik <djedrasik3@gmail.com>
2023-09-06 18:29:03 +0200

    completed homework

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const email = form.elements.email;
+  const password = form.elements.password;
+
+  if (password.value === "" || email.value === "") {
+    console.log("Form fields must not be empty !");
+  } else {
+    const loginData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(loginData);
+  }
+  form.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorBody = document.body;
+const changeColorBtn = document.querySelector(".change-color");
+const visibleColor = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  colorBody.style.backgroundColor = newColor;
+  visibleColor.textContent = newColor;
+});
